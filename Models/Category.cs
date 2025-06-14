1b5681491de296cb9281e7a4c6f7ca4d92b5d3f2
Tal3at10 <mt7993047@gmail.com>
2025-06-14 03:48:22 +0300

    DataAnnotation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required] // means no nullable values in the Name Column
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
 
