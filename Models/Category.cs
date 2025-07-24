6eee66826b67aacb29ce58756f6598f5530d0699
Tal3at10 <mt7993047@gmail.com>
2025-07-24 06:54:09 +0300

    till server side validation '41'

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,9 +8,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required] // means no nullable values in the Name Column
+        [MaxLength(50)] // max length of 50 characters
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 - 100.")]
         public int DisplayOrder { get; set; }
 
     }
