91b7d292ad06ce42a8d5f9c60556476d9011e0e1
Tal3at10 <mt7993047@gmail.com>
2025-07-26 00:07:55 +0300

    EndOf Section4

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.DataAccess;
+using Bulky.Models;
+using Bulky.DataAccess.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
